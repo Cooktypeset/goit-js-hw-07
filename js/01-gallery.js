99d24e9ef0a3d23e4e110ef01ae424a79c9e06cb
Cooktypeset <tovpyhalitva@gmail.com>
2023-05-30 20:25:00 +0300

    newUpd

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+function createImgMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+const imagesMarkup = createImgMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+console.log(galleryItems);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+function onGalleryContainerClick(event) {
+  const isImageEl = event.target.classList.contains("gallery__image");
+
+  if (!isImageEl) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    { onShow: (instance) => 
+      {document.addEventListener("keydown", onEscapeClose);},
+	
+	onClose: (instance) => {
+    document.removeEventListener("keydown", onEscapeClose);
+  }
+});
+
+  instance.show();
+
+  event.preventDefault();
+  
+  function onEscapeClose(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
